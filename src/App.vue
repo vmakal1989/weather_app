6a2f78d2c56fdc61401b9b7661e9bbf9788f7010
Makal Vitali <Makal.V@digdes.com>
2022-05-16 14:52:38 +0300

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+    <main class="container">
+      
+    </main>
+</template>
+
+<script>
+</script>
+
+
+<style>
+.container {
+  text-align: center;
+  margin-top: 60px;
+}
+</style>
